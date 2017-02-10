5b785769e9243cca468a7368200fedd192958d12
matjazonline <matjazonline@gmail.com>
2017-02-10 23:14:40 +0100

    init

---
+++ foundation-responsive-sub-menu.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $(".responsive-menu-icon-show, .responsive-menu-icon-hide").click(function(ev){
+    $(".responsive-menu").toggleClass("responsive-menu-visible")
+    })
+
+})
